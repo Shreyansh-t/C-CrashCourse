2c7187e5ea0cbdc46027ff296a3e4341cf72d3c6
Shreyansh-t <96645080+Shreyansh-t@users.noreply.github.com>
2025-05-12 09:57:32 +0530

    Add ch1/1-1.cpp

---
+++ ch1/1-1.cpp
@@ -0,0 +1,15 @@
+#include <cstdio>
+
+int absolute_value(int x){
+    if(x >= 0){
+        return x;
+    }
+    else{
+        return x*(-1);
+    }
+}
+
+int main(){
+    int my_num = -10;
+    printf("The absolute of %d is %d.\n", my_num, absolute_value(my_num));
+}
