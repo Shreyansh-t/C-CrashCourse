c6b3cbc4d3ec23d8fdf32ab2bf47996b53b418fc
Shreyansh-t <96645080+Shreyansh-t@users.noreply.github.com>
2025-05-19 10:09:54 +0530

    solution for 4-6

---
+++ ch4/4-6.cpp
@@ -0,0 +1,56 @@
+#include <cstdio>
+#include <sys/time.h>
+#include <cstring>
+#include <utility>
+
+struct TimerClass{
+    struct timeval currentTime;
+
+    TimerClass(const char* input_name): name {} {
+        gettimeofday(&currentTime, NULL);
+        timestamp = currentTime.tv_usec;
+        name = new char[100];
+        std::strncpy(name, input_name, 100);
+    }
+
+    TimerClass(const TimerClass &other){
+        timestamp = other.timestamp;
+        name = new char[100];
+        std::strncpy(name, other.name, 100);
+    }
+
+    TimerClass(TimerClass&& other) noexcept{
+        timestamp = other.timestamp;
+        name = other.name;
+        other.name = NULL;
+    }
+
+    ~TimerClass(){
+        gettimeofday(&currentTime, NULL);
+        auto endTime = currentTime.tv_usec;
+        auto finalTime = endTime - timestamp;
+        if(name!=NULL){
+            printf("Timer name: %s\n Time: %ld\n", name, finalTime);
+            delete[] name;
+        }
+    }
+
+    private:
+        long timestamp;
+        char* name;
+};
+
+void func1(TimerClass t){
+    int count = 0;
+    for(int i = 0; i < 100; i++){
+        for(int j = 0; j < 500; j++){
+            count++;
+        }
+    }
+}
+
+int main(){
+    TimerClass t1("first timer");
+    // TimerClass t2{std::move(t1)};
+    func1(std::move(t1));
+}
